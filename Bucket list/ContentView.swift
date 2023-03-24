36f41a4948015964f5d64e15c68f4ad75fd36cde
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-03-24 10:59:12 +0530

    confronting to comparable

--- Bucket list/ContentView.swift
+++ Bucket list/ContentView.swift
@@ -6,18 +6,20 @@
 //
 
 import SwiftUI
-struct User:Identifiable{
+struct User:Identifiable,Comparable{
     let id = UUID()
     let firstName: String
     let lastName: String
+    
+    static func<(lhs: User,rhs:User)->Bool{
+        lhs.lastName<rhs.lastName
+    }
 }
 struct ContentView: View {
     let values = [1,3,4,56,7,2,-7].sorted()
     let users = [User(firstName: "Taylor", lastName: "Swift"),
                  User(firstName: "Liam", lastName: "Payne"),
-                 User(firstName: "Louis", lastName: "Tomilson")].sorted{
-        $0.lastName<$1.lastName
-    }
+                 User(firstName: "Louis", lastName: "Tomilson")].sorted()
     var body: some View {
         Form{
             List(values,id: \.self){
