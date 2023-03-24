75406b3c14e4d3c631d0d7c9dc16eded63e16f3b
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-03-24 10:55:22 +0530

    comparable part 1

--- Bucket list/ContentView.swift
+++ Bucket list/ContentView.swift
@@ -6,14 +6,27 @@
 //
 
 import SwiftUI
-
+struct User:Identifiable{
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+}
 struct ContentView: View {
+    let values = [1,3,4,56,7,2,-7].sorted()
+    let users = [User(firstName: "Taylor", lastName: "Swift"),
+                 User(firstName: "Liam", lastName: "Payne"),
+                 User(firstName: "Louis", lastName: "Tomilson")].sorted{
+        $0.lastName<$1.lastName
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form{
+            List(values,id: \.self){
+                Text(String($0))
+            }
+            
+            List(users) { user in
+                Text("\(user.firstName) \(user.lastName)")
+            }
         }
         .padding()
     }
