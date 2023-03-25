0dea281ec6007db29ecd3e8305a5a7fe5cddc84e
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-03-25 08:09:19 +0530

    Mapkit Intro
    (cherry picked from commit f542f069cd26f12cdce98fe90772157146cf0021)

--- Bucket list/ContentView.swift
+++ Bucket list/ContentView.swift
@@ -4,18 +4,31 @@
 //
 //  Created by Harshit Agarwal on 24/03/23.
 //
-
+import MapKit
 import SwiftUI
-
+struct Location: Identifiable{
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Map(coordinateRegion: $mapRegion,annotationItems: locations){ locations in
+            MapAnnotation(coordinate: locations.coordinate){
+                Circle()
+                    .stroke(.red,lineWidth: 3)
+                    .onTapGesture {
+                        print("Tapped on \(locations.name)")
+                    }
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
