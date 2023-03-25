8b4e8203e6502308516cab8110634ba28092f9c6
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-03-25 07:17:20 +0530

    writing data

--- Bucket list/ContentView.swift
+++ Bucket list/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .onTapGesture {
+                    let str = "Test Message"
+                    let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                    do {
+                        try str.write(to: url,atomically: true,encoding: .utf8)
+                        let input = try String(contentsOf: url)
+                        print(input)
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                }
         }
         .padding()
     }
