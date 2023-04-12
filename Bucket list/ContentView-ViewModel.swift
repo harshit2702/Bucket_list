0aa0cca59598aa577fa034e647e4c043cbe08fd5
Harshit Agarwal <harshitagarwal46446@icloud.com>
2023-04-13 03:42:24 +0530

    Complete

--- Bucket list/ContentView-ViewModel.swift
+++ Bucket list/ContentView-ViewModel.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 import MapKit
+import LocalAuthentication
+
 
 extension ContentView{
+    
     @MainActor class ViewModel: ObservableObject {
+        
         @Published  var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 27.582256, longitude: 77.697083), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 15))
         @Published  private(set) var location: [Location]
         @Published  var selectedPlaces: Location?
+        @Published var isUnlocked = false
         
         let savedPath = FileManager.documentDirectory.appendingPathComponent("SavedPlaces")
         
@@ -46,6 +51,27 @@ extension ContentView{
                 location[index] = newlocation
             }
             save()
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+                let reason = "We need to unlock your data"
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,  localizedReason: reason) { success, authenticateError in
+                    if success {
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    }else{
+                        //Error
+                    }
+                }
+            }else{
+                //No biometric
+            }
         }
         
         
--- Bucket list/ContentView.swift
+++ Bucket list/ContentView.swift
@@ -11,58 +11,68 @@ struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
     
     var body: some View {
-        ZStack {
-            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.location){location in
-                MapAnnotation(coordinate: location.coordinates ){
-                    VStack{
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundColor(.blue)
-                            .frame(width: 44,height: 44)
-                            .background(.white)
-                            .clipShape(Circle())
-                        Text(location.name)
-                            .font(.callout)
-                            .fontWeight(.bold)
+        if viewModel.isUnlocked {
+            ZStack {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.location){location in
+                    MapAnnotation(coordinate: location.coordinates ){
+                        VStack{
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.blue)
+                                .frame(width: 44,height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                            Text(location.name)
+                                .font(.callout)
+                                .fontWeight(.bold)
                             
-                        
-                    }
-                    .onTapGesture {
-                        viewModel.selectedPlaces = location
+                            
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlaces = location
+                        }
                     }
                 }
-            }
                 .edgesIgnoringSafeArea(.all)
-            
-            
-            Circle()
-                .foregroundColor(.blue)
-                .opacity(0.4)
-                .frame(width: 44,height: 44)
-            
-            VStack{
-                Spacer()
-                HStack{
+                
+                
+                Circle()
+                    .foregroundColor(.blue)
+                    .opacity(0.4)
+                    .frame(width: 44,height: 44)
+                
+                VStack{
                     Spacer()
-                    Button{
-                        viewModel.addLocation()
-                    }label: {
-                        Image(systemName: "plus")
+                    HStack{
+                        Spacer()
+                        Button{
+                            viewModel.addLocation()
+                        }label: {
+                            Image(systemName: "plus")
+                                .padding()
+                                .background(.black.opacity(0.4))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(RoundedRectangle(cornerRadius: 15.0))
+                        }
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(.black.opacity(0.4))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(RoundedRectangle(cornerRadius: 15.0))
-                    .padding(.trailing)
+                }
+                
+            }
+            .sheet(item: $viewModel.selectedPlaces){place in
+                EditView(location: place){newlocation in
+                    viewModel.update(newlocation:  newlocation)
                 }
             }
-            
-        }
-        .sheet(item: $viewModel.selectedPlaces){place in
-            EditView(location: place){newlocation in
-                viewModel.update(newlocation:  newlocation)
+        }else{
+            Button("Unlock Places"){
+                viewModel.authenticate()
             }
+            .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
         }
     }
 }
